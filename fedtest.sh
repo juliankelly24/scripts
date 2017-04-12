db2647f9403dab5821b5d5cec47b6e2343187877
Terry Smith <t.smith@aaf.edu.au>
2017-04-12 02:17:13 +0000

    Added some comments and Ideas

--- fedtest.sh
+++ fedtest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 date
 
+# TRS: Need a better way to extract the relevant domain names used
+# by each entity. The active enpoints rather than the entityIDs.
 wget https://md.test.aaf.edu.au/aaf-test-metadata.xml -O dl
 sed '/idp/d' ./dl >> dl2
 grep 'entityID' dl2 >> dl3
@@ -14,11 +16,15 @@ rm dl4
 rm dl5
 
 
+#TRS Find entries that do not have a DNS record for their domain name.
 for LINE in `cat metadata`
 do
   dig $LINE | grep NOERROR 1>/dev/null && echo $LINE >> success || echo $LINE >> fail
 done
 
+#TRS This will never provide any results and all entries do not have a DNS record.
+#TRS Could use host instead of dig in the processing above and use $? to check for 
+#TRS SUCCESS or FAIL
 for LINE2 in `cat fail`
 do
   host $LINE2 >/dev/null
@@ -30,6 +36,7 @@ do
   fi
 done
 
+#TRS Will never find any for retrying.
 for LINE3 in `cat fail`
 do
   ping $LINE3 >/dev/null
@@ -41,6 +48,7 @@ do
   fi
 done
 
+#TRS Same as above.
 for LINE4 in `cat fail`
 do
   curl $LINE4 >/dev/null
@@ -52,6 +60,7 @@ do
   fi
 done
 
+#TRS Same as above
 for LINE5 in `cat fail`
 do
   nmap -Pn $LINE5 >/dev/null
